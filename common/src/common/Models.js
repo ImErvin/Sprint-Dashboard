2e4c8683e5459d24290a4b1e7c317a6b22f6d2b4
eahlehm <ahmed.elhadidy@ericsson.com>
2019-01-30 16:37:45 +0000

    basic models

--- common/src/common/Models.js
+++ common/src/common/Models.js
@@ -9,25 +9,32 @@ define(function(){
       this.members = [];
       this.projects = [];
       this.repos = [];
-   }
+   };
    var Member = function(){
-      this.name;
-      this.email;
-   }
+      this.name='';
+      this.email='';
+   };
    var Project = function (){
        this.name='';
        this.sprints = [];
        this.test = {
          unit: false,
          integration: false
-       }
+       };   
 
-   }
+   };
 
    var Sprint = function(){
      this.points=0;
      this.commitsNo=0;
      this.lineOfCode=0;
-   }
+   };
+
+   return {
+      Team:Team,
+      Member:Member,
+      Project:Project,
+      Sprint:Sprint
+   };
 
 });
