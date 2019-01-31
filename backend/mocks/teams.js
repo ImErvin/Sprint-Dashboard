63dee2c97e7cf6a9d3976eaf9e5085fe06c61344
emanerv <ervin.manutov@ericsson.com>
2019-01-31 08:40:50 +0000

    Added add/delete and update functionaltiy to team.js, add and delete functions may be redundant if decision to handle them on the front end

--- backend/mocks/teams.js
+++ backend/mocks/teams.js
@@ -3,7 +3,7 @@ var Team = require("../models/Team");
 var teams = (() => {
     var tempTeamArr = new Array();
     for(var i = 0; i < 5; i++){
-        tempTeamArr.push(new Team(i));
+        tempTeamArr.push(new Team(i, i, i, i));
     }
 
     return tempTeamArr;
--- backend/models/Team.js
+++ backend/models/Team.js
@@ -12,6 +12,39 @@ Team.prototype = {
         this.members = members;
         this.projects = projects;
         this.repos = repos;
+    },
+
+    addMember: (member) => {
+        this.members.push(member);
+    },
+
+    removeMember: (member) => {
+        var index = this.members.indexOf(member);
+        if(index > -1){
+            this.members.splice(index, 1);
+        }
+    },
+
+    addProject: (project) => {
+        this.projects.push(project);
+    },
+
+    removeProject: (project) => {
+        var index = this.projects.indexOf(project);
+        if(index > -1){
+            this.projects.splice(index, 1);
+        }
+    },
+
+    addRepo: (repo) => {
+        this.repos.push(repo);
+    },
+
+    removeRepo: (repo) => {
+        var index = this.repos.indexOf(repo);
+        if(index > -1){
+            this.repos.splice(index, 1);
+        }
     }
 }
 
